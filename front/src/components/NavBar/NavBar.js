a5aaab32800fb1d145b19b7b1f3ad67d598e7346
PierreWCS <pierrelegrain45@gmail.com>
2020-01-23 09:24:19 +0100

    bfvh,kfg,hgfhtkf

---
+++ front/src/components/NavBar/NavBar.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { NavLink } from 'react-router-dom';
+import './NavBar.css';
+
+const NavBar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const closeMenu = () => {
+    setToggle(false);
+  };
+
+  return (
+    <div>
+      <div className={`${toggle ? 'listNavBar' : 'closedMenuBurger'}`}>
+        <div className="burgerMenuContainer">
+          {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
+          <div
+            onClick={() => setToggle(!toggle)}
+            className={`burgerMenu ${toggle ? 'change' : null}`}
+          >
+            <div className="bar1" />
+            <div className="bar2" />
+            <div className="bar3" />
+          </div>
+        </div>
+      </div>
+      {toggle ? (
+        <div>
+          <NavLink
+            style={{ textDecoration: 'none' }}
+            activeStyle={{ fontWeight: 'bold', color: 'indianred' }}
+            to="/"
+          >
+            <h3 className="itemListNavBar" onClick={closeMenu}>
+              Home
+            </h3>
+          </NavLink>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default NavBar;
