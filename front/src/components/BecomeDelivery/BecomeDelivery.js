4e5a5a54df7137ff502e4f5983c9d3da2c5f189e
PierreWCS <pierrelegrain45@gmail.com>
2020-01-24 17:24:31 +0100

    images clickable and style

--- front/src/components/BecomeDelivery/BecomeDelivery.js
+++ front/src/components/BecomeDelivery/BecomeDelivery.js
@@ -1,54 +1,52 @@
 import React from 'react';
 import './BecomeDelivery.css';
+import NavBar from '../NavBar/NavBar';
 
 const BecomeDelivery = () => {
   return (
     <div className="becomeContainer">
-      <div className="becomePartnairContainer">
-        <h2 style={{ margin: 0 }}>Devenir partenaire DelivGreen ?</h2>
-        <h2>Les étapes</h2>
-        <div className="containerAdhesion">
-          <img
-            className="logoPartenaire"
-            src="https://zupimages.net/up/20/04/77dv.png"
-            alt="logo by Eucalyp"
-          />
-          <h1>Demande d'adhésion</h1>
-        </div>
-        <div className="containerChoice">
-          <img
-            className="logoChoice"
-            src="https://zupimages.net/up/20/04/x3o3.png"
-            alt="logo by Freepik"
-          />
-          <h1>Je choisis les courses qui me conviennent</h1>
-        </div>
-        <div className="containerSalary">
-          <img
-            className="logoSalary"
-            src="https://zupimages.net/up/20/04/21dg.png"
-            alt="logo by Pixel Peeker"
-          />
-          <h1>Je livre la commande et je reçois mon salaire</h1>
-        </div>
+      <NavBar />
+      <h1 style={{ margin: '15px' }}>Devenir partenaire DelivGreen ?</h1>
+      <h1>3 étapes :</h1>
+      <div className="containerAdhesion">
+        <img
+          className="logoPartenaire"
+          src="https://zupimages.net/up/20/04/77dv.png"
+          alt="logo by Eucalyp"
+        />
+        <h3 className="titleSteps">Demande d'adhésion</h3>
+      </div>
+      <div className="containerChoice">
+        <img
+          className="logoChoice"
+          src="https://zupimages.net/up/20/04/x3o3.png"
+          alt="logo by Freepik"
+        />
+        <h3 className="titleSteps">
+          Je choisis les courses qui me conviennent
+        </h3>
+      </div>
+      <div className="containerSalary">
+        <img
+          className="logoSalary"
+          src="https://zupimages.net/up/20/04/21dg.png"
+          alt="logo by Pixel Peeker"
+        />
+        <h3 className="titleSteps">
+          Je livre la commande et je reçois mon salaire
+        </h3>
       </div>
       <h1>Devenir livreur DelivGreen</h1>
-      <div>
-        <img
-          className="particulierVelo"
-          src="https://zupimages.net/up/20/04/mk05.jpg"
-          alt="particulier"
-        />
-        <img
-          className="coursierScooter"
-          src="https://zupimages.net/up/20/04/tzt8.jpg"
-          alt="coursier"
-        />
-        <img
-          className="transporteurImage"
-          src="https://zupimages.net/up/20/04/zt2w.jpg"
-          alt="transporteur"
-        />
+      <div className="liensImagesDelivery">
+        <div className="particulierVelo">
+          <h1 style={{ margin: 0 }}>Particulier</h1>
+        </div>
+        <div className="coursierScooter">
+          <h1>Coursier</h1>
+        </div>
+        <div className="transporteurCamion">
+          <h1>Transporteur</h1>
+        </div>
       </div>
     </div>
   );
