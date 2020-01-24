06a882a88e646ceea282bed9877eeff7ef421a90
Sibel Koc <sibel.koc.pro@outlook.fr>
2020-01-24 11:00:35 +0100

    correction des commentaires

--- front/src/components/contactPage/Form.js
+++ front/src/components/contactPage/Form.js
@@ -3,40 +3,42 @@ import './contactPage.css';
 
 const Form = () => {
   return (
-    <div className="FormContact">
-      <div className="title">Contactez-nous !</div>
-      {/* ton nom */}
-      <div className="select-wrapper">
-        <div className="label"> Nom </div>
-        <input className="textfeildGroup" type="text" />
-      </div>
-      {/* ton prenom */}
+    <div id="all">
+      <div className="FormContact">
+        <div className="title">Contactez-nous !</div>
+        {/* ton nom */}
+        <div className="select-wrapper">
+          <div className="field"> Nom </div>
+          <input className="textfeildGroup" type="text" />
+        </div>
+        {/* ton prenom */}
 
-      <div className="select-wrapper">
-        <div className="label">Prénom</div>
-        <input className="textfeildGroup" type="text" />
+        <div className="select-wrapper">
+          <div className="field">Prénom</div>
+          <input className="textfeildGroup" type="text" />
+        </div>
+        {/* Email */}
+        <div className="select-wrapper">
+          <div className="field">Email</div>
+          <input className="textfeild" type="text" />
+        </div>
+        {/* téléphone */}
+        <div className="select-wrapper">
+          <div className="field">Téléphone</div>
+          <input className="textfeild" type="text" />
+        </div>
+        {/* message */}
+        <div className="select-wrapper">
+          <div className="field">Ton message</div>
+          <input className="textarea" type="text" />
+        </div>
+        <input
+          className="submit"
+          type="submit"
+          value="Envoyer !"
+          placeholder="Envoyer !"
+        />
       </div>
-      {/* Email */}
-      <div className="select-wrapper">
-        <div className="label">Email</div>
-        <input className="textfeild" type="text" />
-      </div>
-      {/* téléphone */}
-      <div className="select-wrapper">
-        <div className="label">Téléphone</div>
-        <input className="textfeild" type="text" />
-      </div>
-      {/* message */}
-      <div className="select-wrapper">
-        <div className="label">Ton message</div>
-        <input className="textarea" type="text" />
-      </div>
-      <input
-        className="submit"
-        type="submit"
-        value="Envoyer !"
-        placeholder="Envoyer !"
-      />
     </div>
   );
 };
