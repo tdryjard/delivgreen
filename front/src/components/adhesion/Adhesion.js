c78c26fa4b9670b69e4e478b4882ee6ebda54c6f
Thomas.dryjard <newwbuiss@gmail.com>
2020-01-25 19:27:16 +0100

    correction name in english, delete adhesion pro et condition avec state

--- front/src/components/adhesion/Adhesion.js
+++ front/src/components/adhesion/Adhesion.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from '../NavBar/NavBar';
 import Footer from '../footer/Footer';
 import './adhesion.css';
 
 const Adhesion = () => {
+  const [pro, setPro] = useState(true);
+
+  setPro(true);
+
   return (
     <div className="contentAdhesion">
       <NavBar />
@@ -12,18 +16,18 @@ const Adhesion = () => {
         <input type="text" name="lastname" placeholder="Nom" />
         <input type="text" name="firstname" placeholder="Prénom" />
         <input type="email" name="email" placeholder="Email" />
-        <input type="text" placeholder="Téléphone" />
-        <div className="localisationAdd">
+        <input type="tel" placeholder="Téléphone" />
+        <div className="locationAdd">
           <input
-            className="localisationInput"
+            className="locationInput"
             type="text"
             name="ville"
             placeholder="Ville"
           />
           <input
-            className="localisationInput"
+            className="locationInput"
             type="text"
-            name="périmètre"
+            name="perimeter"
             placeholder="Périmètre"
           />
           <p className="km">km</p>
@@ -41,6 +45,35 @@ const Adhesion = () => {
             />
           </label>
         </div>
+        {pro ? (
+          <div className="containerPro">
+            <div className="contentFileInput">
+              <label htmlFor="carteIdd" className="label-file">
+                Insérer Kbis de moins de 3 mois
+                <input
+                  className="fileInput"
+                  type="file"
+                  id="carteIdd"
+                  name="CarteIdd"
+                  accept="image/png, image/jpeg"
+                />
+              </label>
+            </div>
+            <input
+              className="contentFileInput"
+              type="text"
+              name="SIRET"
+              placeholder="n° de SIRET"
+            />
+            <input
+              className="contentFileInput"
+              type="text"
+              name="TVA"
+              placeholder="n° de TVA"
+            />
+          </div>
+        ) : null}
+
         <button type="submit" className="btn">
           Envoyer
         </button>
