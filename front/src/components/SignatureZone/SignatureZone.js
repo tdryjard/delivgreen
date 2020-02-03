d0b33ecdcea1e558adbff9c117910e7a4a20b0e7
PierreWCS <pierrelegrain45@gmail.com>
2020-02-03 10:47:49 +0100

    signature zone OK

--- front/src/components/SignatureZone/SignatureZone.js
+++ front/src/components/SignatureZone/SignatureZone.js
@@ -1,19 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './SignatureZone.css';
+import SignatureCanvas from 'react-signature-canvas';
+import useWindowDimensions from '../Dashboard/useWindowDimensions';
 
 const SignatureZone = () => {
+  const [signatureImage, setSignatureImage] = useState(null);
+  const { width } = useWindowDimensions();
+  const [sigPad, setSigPad] = useState({});
+
+  const clear = () => {
+    setSigPad(sigPad.clear);
+  };
+
+  const trim = () => {
+    setSignatureImage(sigPad.getTrimmedCanvas().toDataURL('image/png'));
+  };
+
   return (
-    <div>
-      <h1>Canvas pour signature</h1>
-      <div className="mise-en-page">
-        <div className="bloc-mise-en-page">
-          <h2>Signer</h2>
-          <canvas id="canvas" />
-        </div>
-        <div className="bloc-mise-en-page">
-          <h2>Capture de la signature</h2>
-          <div id="capture" />
-        </div>
+    <div className="signatureZoneContainer">
+      <h1>Signez ci-dessous</h1>
+      <div className="canvasContainer">
+        {width > 1060 ? (
+          <SignatureCanvas
+            penColor="black"
+            canvasProps={{ width: 700, height: 600, className: 'sigCanvas' }}
+            ref={ref => {
+              setSigPad(ref);
+            }}
+          />
+        ) : (
+          <SignatureCanvas
+            penColor="black"
+            canvasProps={{ width: 400, height: 300, className: 'sigCanvas' }}
+            ref={ref => {
+              setSigPad(ref);
+            }}
+          />
+        )}
       </div>
+      <button onClick={clear} type="button">
+        Effacer
+      </button>
+      <button onClick={trim} type="button">
+        Sauvegarder
+      </button>
+      {signatureImage ? (
+        <img
+          className="signatureSave"
+          src={signatureImage}
+          alt="signature png"
+        />
+      ) : null}
     </div>
   );
 };
