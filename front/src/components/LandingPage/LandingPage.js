a0c2d6783aaec7dada4d86f6bdbf964180cfea65
PierreWCS <pierrelegrain45@gmail.com>
2020-02-05 18:07:00 +0100

    customer services section ok style to update

--- front/src/components/LandingPage/LandingPage.js
+++ front/src/components/LandingPage/LandingPage.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faWeightHanging,
-  faMapMarked,
-  faTruckLoading
+  faEnvelope,
+  faCheckCircle,
+  faClock
 } from '@fortawesome/free-solid-svg-icons';
 import './LandingPage.css';
 import Footer from '../footer/Footer';
@@ -36,12 +36,13 @@ const LandingPage = () => {
             <h1 className="titleLandingPage">éco-responsable</h1>
           </div>
           <button type="button" className="buttonHeaderHomePage">
-            Faire livrer un colis
+            Proposer une course
           </button>
         </div>
         <img
           className="headerImageLandingPage"
-          src="https://zupimages.net/up/20/06/5z44.jpg"
+          src="https://zupimages.net/up/20/06/5v8z.jpg"
+          // src="https://zupimages.net/up/20/06/5z44.jpg"
           alt="header"
         />
       </div>
@@ -70,66 +71,94 @@ const LandingPage = () => {
         <h1>3 étapes pour faire livrer votre colis</h1>
         <div className="tripleStepsContainer">
           <div className="iconAndTextThreeSteps">
-            <FontAwesomeIcon
-              className="far fa-3x iconThreeSteps"
-              icon={faWeightHanging}
-            />
+            <h1 style={{ color: '#17B994', fontSize: '64px' }}>1</h1>
             <h3>Je renseigne mon colis</h3>
           </div>
           <div className="iconAndTextThreeSteps">
-            <FontAwesomeIcon
-              className="far fa-3x iconThreeSteps"
-              icon={faMapMarked}
-            />
+            <h1 style={{ color: '#17B994', fontSize: '64px' }}>2</h1>
             <h3>Je choisis un point de départ et d'arrivée</h3>
           </div>
           <div className="iconAndTextThreeSteps">
-            <FontAwesomeIcon
-              className="far fa-3x iconThreeSteps"
-              icon={faTruckLoading}
-            />
+            <h1 style={{ color: '#17B994', fontSize: '64px' }}>3</h1>
             <h3>Mon colis est pris en charge</h3>
           </div>
         </div>
       </div>
 
+      {/*     Customer service     */}
+
+      <div className="customerServiceContainer">
+        <div className="cardCustomerServiceContainer">
+          <h1 className="customerServicesTitle">Deliv'Green à votre écoute</h1>
+          <div className="customerServicesContainer">
+            <div className="itemCustomer">
+              <FontAwesomeIcon
+                icon={faCheckCircle}
+                className="far fa-5x iconThreeSteps"
+              />
+              <p className="textCustomerLandingPage">Livraison satisfait</p>
+              <p className="textCustomerLandingPage">ou remboursé</p>
+            </div>
+            <div className="itemCustomer">
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                className="far fa-5x iconThreeSteps"
+              />
+              <p className="textCustomerLandingPage">Service client</p>
+              <p className="textCustomerLandingPage">24/7</p>
+            </div>
+            <div className="itemCustomer">
+              <FontAwesomeIcon
+                icon={faClock}
+                className="far fa-5x iconThreeSteps"
+              />
+              <p className="textCustomerLandingPage">Réactivité garantie</p>
+              <p className="textCustomerLandingPage">2h maxi</p>
+            </div>
+          </div>
+          <button type="button" className="buttonHeaderHomePage">
+            Contactez-nous
+          </button>
+        </div>
+      </div>
+
       {/*     Become partnaire      */}
 
-      {/* <div className="becomePartnaireSection"> */}
-      {/*  <h1>Devenir partenaire</h1> */}
-      {/*  <div className="becomePartnaireBoxContainer"> */}
-      {/*    <div className="becomePartnaireBox box1"> */}
-      {/*      <div className="textPartnaireBox"> */}
-      {/*        <h3>Coursier</h3> */}
-      {/*        <p> */}
-      {/*          Rentabilisez vos déplacements en regardant si des colis sont sur */}
-      {/*          votre route */}
-      {/*        </p> */}
-      {/*        <button type="button">Devenir coursier</button> */}
-      {/*      </div> */}
-      {/*    </div> */}
-      {/*    <div className="becomePartnaireBox box2"> */}
-      {/*      <div className="textPartnaireBox"> */}
-      {/*        <h3>Coursier</h3> */}
-      {/*        <p> */}
-      {/*          Rentabilisez vos déplacements en regardant si des colis sont sur */}
-      {/*          votre route */}
-      {/*        </p> */}
-      {/*        <button type="button">Devenir coursier</button> */}
-      {/*      </div> */}
-      {/*    </div> */}
-      {/*    <div className="becomePartnaireBox box3"> */}
-      {/*      <div className="textPartnaireBox"> */}
-      {/*        <h3>Coursier</h3> */}
-      {/*        <p> */}
-      {/*          Rentabilisez vos déplacements en regardant si des colis sont sur */}
-      {/*          votre route */}
-      {/*        </p> */}
-      {/*        <button type="button">Devenir coursier</button> */}
-      {/*      </div> */}
-      {/*    </div> */}
-      {/*  </div> */}
-      {/* </div> */}
+      <div className="becomePartnaireSection">
+        <h1>Devenir partenaire</h1>
+        <div className="becomePartnaireBoxContainer">
+          <div className="becomePartnaireBox box1">
+            <div className="textPartnaireBox">
+              <h3>Coursier</h3>
+              <p>
+                Rentabilisez vos déplacements en regardant si des colis sont sur
+                votre route
+              </p>
+              <button type="button">Devenir coursier</button>
+            </div>
+          </div>
+          <div className="becomePartnaireBox box2">
+            <div className="textPartnaireBox">
+              <h3>Livreur</h3>
+              <p>
+                Rentabilisez vos déplacements en regardant si des colis sont sur
+                votre route
+              </p>
+              <button type="button">Devenir coursier</button>
+            </div>
+          </div>
+          <div className="becomePartnaireBox box3">
+            <div className="textPartnaireBox">
+              <h3>Transporteur</h3>
+              <p>
+                Rentabilisez vos déplacements en regardant si des colis sont sur
+                votre route
+              </p>
+              <button type="button">Devenir coursier</button>
+            </div>
+          </div>
+        </div>
+      </div>
 
       {/*     Footer      */}
 
