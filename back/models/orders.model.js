8f1576560ca430a83f5b862d027646ec46e3307f
Sibel Koc <sibel.koc.pro@outlook.fr>
2020-02-18 10:35:36 +0100

    correction conflicts

--- back/models/orders.model.js
+++ back/models/orders.model.js
@@ -14,7 +14,7 @@ const Orders = function(orders) {
 
 Orders.findOrders = result => {
   db.query(
-    `SELECT orders.lngt, orders.height, orders.weight, orders.publish_date, orders.limit_date, orders.price,
+    `SELECT orders.length, orders.id, orders.height, orders.weight, orders.publish_date, orders.limit_date, orders.price,
               orders.start_address_id, start.name AS start_address_name, start.lat AS start_address_lat, start.lng AS start_address_lng,
               orders.end_address_id, end.name AS end_address_name, end.lat AS end_address_lat, end.lng AS end_address_lng
               FROM orders
@@ -30,12 +30,7 @@ Orders.findOrders = result => {
   );
 };
 
-    if (error) {
-      return result(error, null);
-    }
-
-    return result(null, { id: dbResult.insertId, ...newOrders });
-  });
+Orders.updateOrder = (userId, orders, result) => {
   db.query(
     `UPDATE orders SET delivery_man_id = ? WHERE id = ?`,
     [userId, orders],
