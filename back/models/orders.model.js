72e0a4d5f62d382452fa442c4ea432637b8ed1fa
PierreWCS <pierrelegrain45@gmail.com>
2020-02-20 14:55:43 +0100

    commit for changing branch

--- back/models/orders.model.js
+++ back/models/orders.model.js
@@ -25,7 +25,12 @@ Orders.findOrders = result => {
 
 Orders.findOrdersByUser = (userId, result) => {
   db.query(
-    'SELECT orders.publish_date, orders.arrival_date, orders.id from orders where orders.user_id = ?',
+    `SELECT orders.lngt, orders.id, orders.height, orders.weight, orders.publish_date, orders.limit_date, orders.price,
+              orders.start_address_id, start.name AS start_address_name, start.lat AS start_address_lat, start.lng AS start_address_lng,
+              orders.end_address_id, end.name AS end_address_name, end.lat AS end_address_lat, end.lng AS end_address_lng
+              FROM orders
+              JOIN address AS start ON orders.start_address_id = start.id
+              JOIN address AS end ON orders.end_address_id = end.id  where orders.user_id = ?`,
     userId,
     (error, dbResult) => {
       if (error) {
--- front/src/components/Dashboard/MyOrders.js
+++ front/src/components/Dashboard/MyOrders.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faCircle } from '@fortawesome/free-solid-svg-icons';
 import './MyOrders.css';
-// import axios from 'axios';
+import axios from 'axios';
 import NavBarDashboard from './NavBarDashboard';
 import HeaderDashboard from './HeaderDashboard';
 import useWindowDimensions from './useWindowDimensions';
@@ -11,83 +11,25 @@ import NavBarDashboardMobile from './NavBarDashboardMobile';
 const MyOrders = () => {
   const { width } = useWindowDimensions();
   const [toggleNavBarMobile, setToggleNavBarMobile] = useState(false);
-  // const [clientOrders, setClientOrders] = useState(null);
+  const [clientOrders, setClientOrders] = useState(null);
 
-  // const getProducts = () => {
-  //   // let getUrl = `http://localhost:8000/api/orders/${clientId}`;
-  //   const getUrl = `http://localhost:8000/api/orders/1`;
-  //   axios
-  //     .get(getUrl)
-  //     .then(result => result.data)
-  //     .then(data => {
-  //       const stockOrders = data;
-  //       console.log(stockOrders);
-  //       setClientOrders(stockOrders);
-  //     });
-  // };
+  const getProducts = () => {
+    // let getUrl = `http://localhost:8000/api/orders/${clientId}`;
+    const userId = 1;
+    const getUrl = `http://localhost:8000/api/orders?userId=${userId}`;
+    axios
+      .get(getUrl)
+      .then(result => result.data)
+      .then(data => {
+        const stockOrders = data;
+        console.log(stockOrders);
+        setClientOrders(stockOrders);
+      });
+  };
 
-  const orderExample = [
-    {
-      number: '253',
-      pick_up_date: '13/01/2020',
-      departure: '18 rue de la Choppe 45000 Orléans',
-      departure_date: '18/01/2020',
-      arrival: '12 rue de la Gare 45000 Orléans',
-      limit_date: '24/01/2020',
-      weight: 170,
-      length: 190,
-      height: 155,
-      status: 'Pris en charge'
-    },
-    {
-      number: '898',
-      pick_up_date: '',
-      departure: '18 rue de la Choppe 45000 Orléans',
-      departure_date: '18/01/2020',
-      arrival: '12 rue de la Gare 45000 Orléans',
-      limit_date: '12/02/2020',
-      weight: 170,
-      length: 190,
-      height: 155,
-      status: 'Acceptée'
-    },
-    {
-      number: '853',
-      pick_up_date: '13/01/2020',
-      departure: '18 rue de la Choppe 45000 Orléans',
-      departure_date: '18/01/2020',
-      arrival: '12 rue de la Gare 45000 Orléans',
-      limit_date: '24/01/2020',
-      weight: 170,
-      length: 190,
-      height: 155,
-      status: 'Pris en charge'
-    },
-    {
-      number: '253',
-      pick_up_date: '13/01/2020',
-      departure: '18 rue de la Choppe 45000 Orléans',
-      departure_date: '18/01/2020',
-      arrival: '12 rue de la Gare 45000 Orléans',
-      limit_date: '24/01/2020',
-      weight: 170,
-      length: 190,
-      height: 155,
-      status: 'Acceptée'
-    },
-    {
-      number: '256',
-      pick_up_date: '13/01/2020',
-      departure: '18 rue de la Choppe 45000 Orléans',
-      departure_date: '18/01/2020',
-      arrival: '12 rue de la Gare 45000 Orléans',
-      limit_date: '24/01/2020',
-      weight: 56,
-      length: 80,
-      height: 125,
-      status: 'Pris en charge'
-    }
-  ];
+  useEffect(() => {
+    getProducts();
+  }, []);
 
   return (
     <div className="mySpaceMainContainer">
@@ -124,7 +66,9 @@ const MyOrders = () => {
                 icon={faCircle}
               />
             </div>
-            {/* <button onClick={getProducts}>Click</button> */}
+            <button type="button" onClick={getProducts}>
+              Click
+            </button>
           </div>
           <div className="ordersCardContainer">
             <div className="myOrdersContainerList">
@@ -146,36 +90,38 @@ const MyOrders = () => {
                     </th>
                   </tr>
                 </thead>
-                {orderExample.map(order => {
-                  return (
-                    <tbody className="itemsContainerOrder">
-                      <td className="itemTableOrders">
-                        <p className="itemListOrders">{order.number}</p>
-                      </td>
-                      <td className="itemTableOrders">
-                        <p className="itemListOrders">
-                          {order.status === 'Pris en charge' ? (
-                            <FontAwesomeIcon
-                              style={{ color: 'orange' }}
-                              icon={faCircle}
-                            />
-                          ) : (
-                            <FontAwesomeIcon
-                              style={{ color: '#3c9d9b' }}
-                              icon={faCircle}
-                            />
-                          )}
-                        </p>
-                      </td>
-                      <td className="itemTableOrders">
-                        <p className="itemListOrders">{order.limit_date}</p>
-                      </td>
-                      <td className="itemTableOrders">
-                        <p className="buttonActionOrder">Détails</p>
-                      </td>
-                    </tbody>
-                  );
-                })}
+                {clientOrders
+                  ? clientOrders.map(order => {
+                      return (
+                        <tbody className="itemsContainerOrder">
+                          <td className="itemTableOrders">
+                            <p className="itemListOrders">{order.id}</p>
+                          </td>
+                          <td className="itemTableOrders">
+                            <p className="itemListOrders">
+                              {order.status === 'Pris en charge' ? (
+                                <FontAwesomeIcon
+                                  style={{ color: 'orange' }}
+                                  icon={faCircle}
+                                />
+                              ) : (
+                                <FontAwesomeIcon
+                                  style={{ color: '#3c9d9b' }}
+                                  icon={faCircle}
+                                />
+                              )}
+                            </p>
+                          </td>
+                          <td className="itemTableOrders">
+                            <p className="itemListOrders">{order.limit_date}</p>
+                          </td>
+                          <td className="itemTableOrders">
+                            <p className="buttonActionOrder">Détails</p>
+                          </td>
+                        </tbody>
+                      );
+                    })
+                  : null}
               </table>
             </div>
           </div>
